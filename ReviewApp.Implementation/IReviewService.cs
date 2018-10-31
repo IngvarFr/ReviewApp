12f6fc718977624b077bc8a5e07f1457fb5e1230
Ingvar Friðbjörnsson <ingvarfr@gmail.com>
2018-10-31 19:52:43 +0100

    Comments

--- ReviewApp.Implementation/IReviewService.cs
+++ ReviewApp.Implementation/IReviewService.cs
@@ -10,37 +10,81 @@ namespace ReviewApp.Implementation
         // The container for the reviews parsed into the application.
         IEnumerable<Review> Reviews { get; set; }
 
-        // Returns the number of reviews from the reviewer.
+        /// <summary>
+        /// Returns the number of reviews from the reviewer.
+        /// </summary>
+        /// <param name="reviewer"></param>
+        /// <returns></returns>
         int ReviewsFromReviewer(int reviewer);
 
-        // Returns the average grade the reviewer has given.
+        /// <summary>
+        /// Returns the average grade the reviewer has given.
+        /// </summary>
+        /// <param name="reviewer"></param>
+        /// <returns></returns>
         double AverageGradeFromReviewer(int reviewer);
 
-        // Returns the number of times a reviewer has given a certain grade.
+        /// <summary>
+        /// Returns the number of times a reviewer has given a certain grade.
+        /// </summary>
+        /// <param name="reviewer"></param>
+        /// <param name="grade"></param>
+        /// <returns></returns>
         int GradeCountFromReviewer(int reviewer, int grade);
 
-        // Returns the amount of reviewers who have graded a certain movie.
+        /// <summary>
+        /// Returns the amount of reviewers who have graded a certain movie.
+        /// </summary>
+        /// <param name="movie"></param>
+        /// <returns></returns>
         int ReviewerCountForMovie(int movie);
 
-        // Returns the average grade of a certain movie.
+        /// <summary>
+        /// Returns the average grade of a certain movie.
+        /// </summary>
+        /// <param name="movie"></param>
+        /// <returns></returns>
         double AverageGradeOfMovie(int movie);
 
-        // Returns the number of times a movie has been given a certain grade.
+        /// <summary>
+        /// Returns the number of times a movie has been given a certain grade.
+        /// </summary>
+        /// <param name="movie"></param>
+        /// <param name="grade"></param>
+        /// <returns></returns>
         int GradeCountForMovie(int movie, int grade);
 
-        // Returns the 10 movies which have the most 5 grades.
+        /// <summary>
+        /// Returns the 10 movies which have the most 5 grades.
+        /// </summary>
+        /// <returns></returns>
         List<int> MostTopGradesMovies();
 
-        // Returns the 10 Reviewers with the most reviews.
+        /// <summary>
+        /// Returns the 10 Reviewers with the most reviews.
+        /// </summary>
+        /// <returns></returns>
         List<int> TopReviewers();
 
-        // Returns a number of movies with the highest average grade. 
+        /// <summary>
+        /// Returns a number of movies with the highest average grade.
+        /// </summary>
+        /// <param name="count"></param>
+        /// <returns></returns>
         List<int> TopMovies(int count);
 
-        // Returns a list of movies a certain reviewer has reviewed.
+        /// <summary>
+        /// Returns a list of movies a certain reviewer has reviewed.
+        /// </summary>
+        /// <param name="reviewer"></param>
+        /// <returns></returns>
         List<int> MoviesByReviewer(int reviewer);
 
-        // Returns a list of reviewers which have reviewed a movie
+        /// <summary>
+        /// Returns a list of reviewers which have reviewed a movie.
+        /// </summary>
+        /// <param name="movie"></param>
+        /// <returns></returns>
         List<int> ReviewersOfMovie(int movie);
     }
 }
